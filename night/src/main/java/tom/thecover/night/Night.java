3800517ac5b1225fb4385951805df2247fa43f17
tom_dangtao <tom@thecover.cn>
2017-11-02 17:56:07 +0800

    update

--- night/src/main/java/tom/thecover/night/Night.java
+++ night/src/main/java/tom/thecover/night/Night.java
@@ -10,6 +10,7 @@ import android.widget.Toast;
 public class Night {
 
     public static void say(Context context) {
+
         Toast.makeText(context, "Good tom.thecover.night.Night, Tom", Toast.LENGTH_SHORT).show();
     }
 }
